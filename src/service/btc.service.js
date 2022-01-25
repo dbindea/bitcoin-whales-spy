e43c8f22e98badd5cb0e7e3df30e915b8eed6833
Dorel Bindea <dbindea@gmail.com>
2022-01-25 04:33:07 +0100

    log

--- src/service/btc.service.js
+++ src/service/btc.service.js
@@ -33,20 +33,61 @@ export default class BitcoinService {
     };
 
     addresses.forEach((address) => {
+      scrapeIt(config.BTC_URL + address, callback).then(({ data, response }) => {
+        console.log(data.page);
+      });
+      // promises.push(scrapeIt(config.BTC_URL + address, callback));
+    });
+
+    /* const promisesResponse = await Promise.all(promises);
+
+    promisesResponse.forEach((array, index) => {
+      if (array && array.data && array.data.page) {
+        result.push({
+          asset: 'BTC',
+          address: addresses[index],
+          transactions: array.data.page,
+          currency: 'USD',
+        });
+      }
+    });
+
+    return await Promise.resolve(result); */
+  }
+
+  async _scanBtc() {
+    const promises = [];
+    const result = [];
+
+    const addresses = this.utilsService.splitData(config.BTC_ADDRESSES);
+    const callback = {
+      page: {
+        listItem: 'tr.trb',
+        data: {
+          block: 'a',
+          time: {
+            selector: 'td span.muted.utc.hidden-desktop',
+            convert: (date) => new Date(date),
+          },
+          amount: {
+            selector: 'td:nth-child(3n) span',
+            convert: (x) => Number(x.split('BTC')[0].trim().replace(',', '')),
+          },
+          price: {
+            selector: 'td:nth-child(5n)',
+            convert: (x) => Number(x.split('@')[1].trim().slice(1).replace(',', '')),
+          },
+        },
+      },
+    };
+
+    addresses.forEach((address) => {
       promises.push(scrapeIt(config.BTC_URL + address, callback));
     });
 
     const promisesResponse = await Promise.all(promises);
 
     promisesResponse.forEach((array, index) => {
-
-      // console.log('///array response ' + index, array);
-      console.log('--->> data ' + index, Object.keys(array.data.page));
-      // console.log('--->> $ ' + index, Object.keys(array.$));
-      // console.log('--->> response ' + index, Object.keys(array.response));
-      // console.log('--->> body ' + index, Object.keys(array.body));
-      // console.log('///page response ' + index, array.data.page);
-
       if (array && array.data && array.data.page) {
         result.push({
           asset: 'BTC',
